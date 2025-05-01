bd371a703ca9c9ead9f0211a258ef761985ca860
Jon Pearce <jonpea@gmail.com>
2025-04-30 22:15:24 -0400

    Consistent variable names

--- script.py
+++ script.py
@@ -12,26 +12,26 @@ from polars import DataFrame, Series, col
 base_power = 1000
 
 # Network data
-nodes = DataFrame(
+buses = DataFrame(
     [
-        {"id": "N1", "load": 0},
-        {"id": "N2", "load": 0},
-        {"id": "N3", "load": 150},
+        {"id": "B1", "load": 0},
+        {"id": "B2", "load": 0},
+        {"id": "B3", "load": 150},
     ]
 )
 generators = DataFrame(
     [  # excludes "capacity", "cost"
-        {"id": "G1", "node_id": "N1"},
-        {"id": "G2", "node_id": "N2"},
-        {"id": "G3", "node_id": "N1"},
+        {"id": "G1", "node_id": "B1"},
+        {"id": "G2", "node_id": "B2"},
+        {"id": "G3", "node_id": "B1"},
     ]
 )
 lines = DataFrame(
     # fmt: off
     [
-        {"from_node_id": "N1", "to_node_id": "N2", "susceptance": 0.25, "capacity": 30.},
-        {"from_node_id": "N1", "to_node_id": "N3", "susceptance": 0.25, "capacity": 300.},
-        {"from_node_id": "N2", "to_node_id": "N3", "susceptance": 0.25, "capacity": 300.},
+        {"from_node_id": "B1", "to_node_id": "B2", "susceptance": 0.25, "capacity": 30.},
+        {"from_node_id": "B1", "to_node_id": "B3", "susceptance": 0.25, "capacity": 100.},
+        {"from_node_id": "B2", "to_node_id": "B3", "susceptance": 0.25, "capacity": 100.},
     ]
     # fmt: on
 )
@@ -47,7 +47,7 @@ offers = DataFrame(
 ).sort(by=["generator_id", "price"])
 
 # Index sets
-Nodes = range(nodes.height)
+Buses = range(buses.height)
 Lines = range(lines.height)
 Offers = range(offers.height)
 Generators = range(generators.height)
@@ -67,7 +67,7 @@ def flow_bounds(model: Model, ell: int) -> tuple[float, float]:
 # Variables
 model.p = pyo.Var(Offers, bounds=supply_bounds)
 model.f = pyo.Var(Lines, bounds=flow_bounds)
-model.theta = pyo.Var(Nodes, bounds=(-math.pi, +math.pi))
+model.theta = pyo.Var(Buses, bounds=(-math.pi, +math.pi))
 model.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
 
 # Objective
@@ -77,26 +77,26 @@ model.total_cost = pyo.Objective(
 )
 
 
-def network_incidence(n: int, ell: int) -> int:
-    if nodes[n, "id"] == lines[ell, "from_node_id"]:
+def network_incidence(b: int, ell: int) -> int:
+    if buses[b, "id"] == lines[ell, "from_node_id"]:
         return -1
-    if nodes[n, "id"] == lines[ell, "to_node_id"]:
+    if buses[b, "id"] == lines[ell, "to_node_id"]:
         return +1
     else:
         return 0
 
 
-def supply_incidence(n: int, g: int, o: int) -> bool:
-    node_generator: bool = nodes[n, "id"] == generators[g, "node_id"]
+def supply_incidence(b: int, g: int, o: int) -> bool:
+    node_generator: bool = buses[b, "id"] == generators[g, "node_id"]
     generator_offer: bool = generators[g, "id"] == offers[o, "generator_id"]
     return node_generator and generator_offer
 
 
-def balance_rule(model: Model, n: int) -> EqualityExpression:
+def balance_rule(model: Model, b: int) -> EqualityExpression:
     return (
-        sum(model.p[o] for g in Generators for o in Offers if supply_incidence(n, g, o))
-        + sum(network_incidence(n, ell) * model.f[ell] for ell in Lines)
-        == nodes[n, "load"]
+        sum(model.p[o] for g in Generators for o in Offers if supply_incidence(b, g, o))
+        + sum(network_incidence(b, ell) * model.f[ell] for ell in Lines)
+        == buses[b, "load"]
     )
 
 
@@ -106,15 +106,15 @@ def flow_rule(model: Model, ell: int) -> EqualityExpression:
             base_power
             * sign
             * lines[ell, "susceptance"]
-            * model.theta[n]
-            for n in Nodes
-            if (sign := network_incidence(n, ell))
+            * model.theta[b]
+            for b in Buses
+            if (sign := network_incidence(b, ell))
         )
         == model.f[ell]
     )
 
 
-model.balance = pyo.Constraint(Nodes, rule=balance_rule)
+model.balance = pyo.Constraint(Buses, rule=balance_rule)
 model.flow = pyo.Constraint(Lines, rule=flow_rule)
 model.reference_angle = pyo.Constraint(expr=model.theta[0] == 0.0)
 
@@ -123,12 +123,12 @@ results = solver.solve(model, tee=True)  # tee output to console
 
 supply = [pyo.value(model.p[o]) for o in Offers]
 flow = [pyo.value(model.f[ell]) for ell in Lines]
-angles_deg = [pyo.value(model.theta[n]) * 180 / math.pi for n in Nodes]
-prices = [model.dual[model.balance[n]] for n in Nodes]
+angles_deg = [pyo.value(model.theta[b]) * 180 / math.pi for b in Buses]
+prices = [model.dual[model.balance[b]] for b in Buses]
 
 offers = offers.with_columns([Series("supply", supply)])
 lines = lines.with_columns([Series("flow", flow)])
-nodes = nodes.with_columns([Series("theta_deg", angles_deg), Series("price", prices)])
+buses = buses.with_columns([Series("theta_deg", angles_deg), Series("price", prices)])
 
 supply_prices = offers.group_by("generator_id").agg(col("price"))
 supply_totals = offers.group_by("generator_id").agg(col("supply").sum())
@@ -146,7 +146,7 @@ generators = generators.with_columns(
 
 print("offers -", offers)
 print("lines -", lines)
-print("nodes -", nodes)
+print("buses -", buses)
 print("generators -", generators)
 
 
@@ -154,16 +154,16 @@ def mapvalues(f, keys, *values) -> dict:
     return dict(zip(keys, map(f, *values)))
 
 
-def offset(pos: dict, dx: float, dy: float) -> dict:
+def offset(positions: dict, dx: float, dy: float) -> dict:
     dxy = np.array([dx, dy])
-    return {node: xy + dxy for node, xy in pos.items()}
+    return {id: xy + dxy for id, xy in positions.items()}
 
 
 network = nx.DiGraph()
 
-network.add_nodes_from(nodes["id"])
+network.add_nodes_from(buses["id"])
 network.add_nodes_from(generators["id"])
-bus_color = ["lightblue" for _ in nodes["id"]]
+bus_color = ["lightblue" for _ in buses["id"]]
 generator_color = ["lightgreen" for _ in generators["id"]]
 node_color = bus_color + generator_color
 
@@ -173,7 +173,7 @@ network.add_edges_from(zip(generators["id"], generators["node_id"]))
 # connection_color = tags(generators["id"], "gray")
 # edge_color = line_color + connection_color
 
-nodal_price_labels = mapvalues("${:.2f}/MWh".format, nodes["id"], nodes["price"])
+nodal_price_labels = mapvalues("${:.2f}/MWh".format, buses["id"], buses["price"])
 offer_price_labels = mapvalues(
     "${:}/MWh".format, generators["id"], generators["prices"]
 )
